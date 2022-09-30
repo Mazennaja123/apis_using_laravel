4be19ffed7a02d9b43cc9362bb37274e2353a1bb
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-30 19:10:10 +0300

    Adding Sorted string route

--- app/Http/Controllers/algorithm_controller.php
+++ app/Http/Controllers/algorithm_controller.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Http\controllers\algorithm_controller;
 class algorithm_controller extends Controller
 {
     //
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\controllers\algorithm_controller;
 
-Route::get("/test",function(){
-  return "Testing route";
-});
+Route::get("/algorithms",[algorithm_controller::class,'get_sorted_string']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
