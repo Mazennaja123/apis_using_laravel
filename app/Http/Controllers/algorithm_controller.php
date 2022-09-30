4e38b3eba8550ea2450c376591839950a25f65f3
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-30 20:52:03 +0300

    Creating sort string feature V1

--- app/Http/Controllers/algorithm_controller.php
+++ app/Http/Controllers/algorithm_controller.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\controllers\algorithm_controller;
+
 class algorithm_controller extends Controller
 {
-    //
+
+    //Sort String case sensitive with numbers
+    function get_sorted_string($unsorted_string){
+
+                   $s1 =[];
+                   $s2=[];
+                   $i=0;
+                   $size=strlen($unsorted_string);
+                   for($i; $i<$size; $i++){
+                          if($unsorted_string[$i].charCodeAt(0) > "a".charCodeAt(0) && unsorted_string[$i].charCodeAt(0) <= "z".charCodeAt(0))
+                                $s1.push($unsorted_string[$i]);
+                          if ($unsorted_string[$i].charCodeAt(0) > "A".charCodeAt(0) && unsorted_string[$i].charCodeAt(0) <= "Z".charCodeAt(0))
+                                $s2.push($unsorted_string[$i]);
+                      }
+                  //sorting the arrays for further comparison
+                  $s1.sort();
+                  $s2.sort();
+
+                  $j=0;
+                  $i=0;
+                  $k=0;
+                  //Sort the arrays as needed in terms of case sensitivity
+                  for($k=0;$k<$unsorted_string.count();$k++){
+
+                          if($unsorted_string[$k].charCodeAt(0) > "a".charCodeAt(0) && $unsorted_string[$k].charCodeAt(0) <= "z".charCodeAt(0))
+                           {
+                                $unsorted_string[$k]=$s1[$i];
+                                $i++;
+                           }
+
+                           elseif ($unsorted_string[$k].charCodeAt(0) > "A".charCodeAt(0) && $unsorted_string[$k].charCodeAt(0) <= "Z".charCodeAt(0))
+                            {
+                                 $unsorted_string[$k]=$s2[$j];
+                                 $j++;
+                            }
+                  }
+
+                  return $unsorted_string.join("");
+                }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\algorithm_controller;
 
-Route::get("/algorithms",[algorithm_controller::class,'get_sorted_string']);
+Route::get("/algorithms/{unsorted_string}",[algorithm_controller::class,'get_sorted_string']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
